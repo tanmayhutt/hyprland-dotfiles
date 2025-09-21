5a15f9499469e82e52bbc26f0372fa07d80471a2
tanmayhutt <tiwaritanmay1021@gmail.com>
2025-09-21 21:46:27 +0530

    📦 Auto update: 2025-09-21 21:46:27

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ ln -sf "$DOTFILES_DIR/.config/waybar" "$HOME/.config/waybar"
 ln -sf "$DOTFILES_DIR/.config/hyprlock" "$HOME/.config/hyprlock"
 ln -sf "$DOTFILES_DIR/.config/cava" "$HOME/.config/cava"
 ln -sf "$DOTFILES_DIR/.config/wofi" "$HOME/.config/wofi"
-
+ln -sf "$DOTFILES_DIR/.config/kitty" "$HOME/.config/kitty"
 
 ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
 ln -sf "$DOTFILES_DIR/.zprofile" "$HOME/.zprofile"
